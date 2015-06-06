a9a23fcff86e2b68e941e275579bb47a07206951
Will-Lo <will-i-am_95@hotmail.com>
2015-06-06 15:50:05 -0400

    Incomplete implementation of recipe api
    So apparently the InputScreen thing doesn't update values real time or
    something (prob b/c its init). I'll look at it later. If you think you
    know what to do go ahead because I've been stuck for a while. I made the
    code functional by making the api call a hard coded ingredient

--- RecipeApplication/RecipeApp.py
+++ RecipeApplication/RecipeApp.py
@@ -3,55 +3,55 @@ import json
 
 
 def call_api(ingredient):
-    app_id = '089eb38f'
-    app_key = '1c85c101570a08bde8d50b217d7ca2d9'
-    info = {'q': ingredient, 'app_id': app_id, 'app_key': app_key}
-    response = requests.post('https://api.edamam.com/search', data=info)
-    j = json.loads(response.text)
-    recipe_list = j['hits']
-    return recipe_list
+	app_id = '089eb38f'
+	app_key = '1c85c101570a08bde8d50b217d7ca2d9'
+	info = {'q': ingredient, 'app_id': app_id, 'app_key': app_key}
+	response = requests.post('https://api.edamam.com/search', data=info)
+	j = json.loads(response.text)
+	recipe_list = j['hits']
+	return recipe_list
 
 
 def get_recipe_title(recipe_list):
-    recipe_names = []
-    for title in recipe_list:
-        recipe = title['recipe']
-        recipe_names.append(recipe['label'])
-    return recipe_names
+	recipe_names = []
+	for title in recipe_list:
+		recipe = title['recipe']
+		recipe_names.append(recipe['label'])
+	return recipe_names
 
 
 def choose_recipe(recipe_list):
-    choice = input("Call index # for recipe you like ")
-    chosen_recipe = recipe_list[choice]
-    return chosen_recipe
+	choice = input("Call index # for recipe you like ")
+	chosen_recipe = recipe_list[choice]
+	return chosen_recipe
 
 
 def get_recipe_ingredients(chosen_recipe):
-    r = chosen_recipe['recipe']
-    ingredient_list = r['ingredients']
-    ingredient_info = {}
-    for item in ingredient_list:
-        food_info = item['food']
-        units = item['measure']
-        amount = str(item['quantity']) + " " + str(units['label'])
-        ingredient_info.update({food_info['label']: amount})
-    return ingredient_info
+	r = chosen_recipe['recipe']
+	ingredient_list = r['ingredients']
+	ingredient_info = {}
+	for item in ingredient_list:
+		food_info = item['food']
+		units = item['measure']
+		amount = str(item['quantity']) + " " + str(units['label'])
+		ingredient_info.update({food_info['label']: amount})
+	return ingredient_info
 
 
 def get_difficulty(recipe_list):
-    difficulty_list = []
-    for recipe_title in recipe_list:
-        recipe = recipe_title['recipe']
-        difficulty_list.append(recipe['level'])
-    return difficulty_list
+	difficulty_list = []
+	for recipe_title in recipe_list:
+		recipe = recipe_title['recipe']
+		difficulty_list.append(recipe['level'])
+	return difficulty_list
 
 
 def get_image(recipe_list):
-    image_list = []
-    for recipe_title in recipe_list:
-        recipe = recipe_title['recipe']
-        image_list.append(recipe['image'])
-    return image_list
+	image_list = []
+	for recipe_title in recipe_list:
+		recipe = recipe_title['recipe']
+		image_list.append(recipe['image'])
+	return image_list
 
 
 import kivy
@@ -64,60 +64,66 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 
 class SearchResult(Widget):
-    pass
+	pass
 
 
 class Textbox(TextInput):
-    def __init__(self):
-        # ensures that important stuff isn't overwritten
-        super(Textbox, self).__init__()
+	def __init__(self):
+		# ensures that important stuff isn't overwritten
+		super(Textbox, self).__init__()
 
-        self.info = TextInput(
-            multiline=False,
-            size_hint = (0.3,0.1),
-            pos_hint = {'center_x':0.5, 'center_y': 0.6}
-        )
-    def on_enter(instance, value):
-        print value.text
-
+		self.info = TextInput(
+			multiline=False,
+			size_hint = (0.3,0.1),
+			pos_hint = {'center_x':0.5, 'center_y': 0.6}
+		)
+	def on_enter(instance, value):
+		print value.text
+		return value.text
 
 class SearchButton(FloatLayout):
-    def __init__(self):
-        # ensures that important stuff isn't overwritten
-        super(SearchButton, self).__init__()
+	def __init__(self):
+		# ensures that important stuff isn't overwritten
+		super(SearchButton, self).__init__()
 
-        self.info = Button(
-            text="search",
-            size_hint=(0.2, 0.1),
-            pos_hint={'center_x': 0.5, 'center_y': 0.3}
-        )
-    def on_touch_down(self, touch):
-        if self.collide_point:
-            print('Touched!')
-	
+		self.info = Button(
+			text="search",
+			size_hint=(0.2, 0.1),
+			pos_hint={'center_x': 0.5, 'center_y': 0.3}
+		)
+		self.food = ''
+		
+	def on_touch_down(self, touch):
+		if self.collide_point:
+			print 'Touched!'
+			recipe_list = call_api(self.food)
+			recipe_title = get_recipe_title(recipe_list)
+			print recipe_title
 
 
 class InputScreen(FloatLayout):
-    def __init__(self):
-        # ensures that important stuff isn't overwritten
-        super(InputScreen, self).__init__()
+	def __init__(self):
+		# ensures that important stuff isn't overwritten
+		super(InputScreen, self).__init__()
 
-        button = SearchButton()
-        searchbox = Textbox()
+		#create objects
+		button = SearchButton()
+		searchbox = Textbox()
 		
-        self.add_widget(button.info)
-        self.add_widget(searchbox.info)
-        
-        searchbox.info.bind(on_text_validate=searchbox.on_enter)
-        button.info.bind(on_release=button.on_touch_down)
+		#add the widgets above
+		self.add_widget(button.info)
+		self.add_widget(searchbox.info)
 		
-		
+		ingredient = searchbox.info.bind(on_text_validate=searchbox.on_enter)
 
+		button.food = 'banana'
+		button.info.bind(on_release=button.on_touch_down)
+		
+		
 class RecipeApp(App):
-    def build(self):
-        return InputScreen()
-
+	def build(self):
+		return InputScreen()
 		
 
 if __name__ == '__main__':
-    RecipeApp().run()
+	RecipeApp().run()
